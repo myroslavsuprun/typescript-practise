1050420a02224bb4dd6ac84f66c4ce2150823d5e
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-02 20:08:58 +0200

    Completed task 4

--- src/index.ts
+++ src/index.ts
@@ -1,111 +1,60 @@
-let age: number;
+class Key {
+    private signature: number;
 
-age = 50;
+    constructor() {
+        this.signature = Math.random();
+    }
 
-let nameString: string;
-
-nameString = "Max";
-
-let toggle: boolean;
-
-toggle = true;
-
-let empty: null;
-
-empty = null;
-
-let notInitialize: undefined;
-
-let callback: (a: number) => number;
-
-callback = (a) => 100 + a;
-
-let anything: any;
-
-anything = "text";
-anything = {};
-
-// ----------------------------------------------
-
-let some: unknown;
-some = "text";
-
-let str: string;
-
-
-if (typeof some === "string") {
-    str = some;
-}
-
-// ----------------------------------------------
-
-let person: [string, number];
-
-person = ["Max", 21];
-
-// ----------------------------------------------
-
-enum Status { LOADING, READY };
-
-const page = {
-    load: Status.READY,
-}
-
-if (page.load === Status.LOADING) {
-    console.log('loading');
-} else if (page.load === Status.READY) {
-    console.log('Ready');
-}
-
-
-// ----------------------------------------------
-
-let variable: string | number;
-
-variable = "string";
-variable = 1;
-
-let btnStatus: 'enable' | 'disable';
-
-function message(message: string): void {
-    console.log(message);  
-};
-
-function calc(num1: number, num2: number): number {
-    return num1 + num2;
-};
-
-function customError(): never {
-    throw new Error('Error');
-};
-
-type page = {
-    title: string;
-    likes: number;
-    accounts?: string[];
-    status: 'close' | 'open';
-    details?: {
-        createAt: Date;
-        updateAt: Date;
+    public getSignature(): number {
+        return this.signature;
     }
 }
 
-const page1: page = {
-    title: 'The awesome page',
-    likes: 100,
-    accounts: ['Max', 'Anton', 'Nikita'],
-    status: 'open',
-    details: {
-        createAt: new Date('2021-01-01'),
-        updateAt: new Date('2021-05-01'),
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+
+    public getKey(): Key {
+        return this.key;
     }
 }
 
-const page2: page = {
-    title: 'Python or Js',
-    likes: 5,
-    accounts: ['Alex'],
-    status: 'close'
+
+abstract class House {
+    protected door = false;
+    private tentants: Person[] = [];
+    
+    constructor(protected key: Key) {
+        this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (!this.door) {
+            throw new Error('The door is shut');
+        }
+
+        this.tentants.push(person);
+        console.log('You have successfully come in');
+    }
+
+    abstract openDoor(key: Key): boolean;
 }
 
-console.log(new Date('2021-01-01'));
+class MyHouse extends House {
+    public openDoor(key: Key) {
+        if (key.getSignature() !== this.key.getSignature()) {
+            throw new Error("The key hasn't matched");
+        }
+        return this.door = true;
+    }
+}
+
+
+
+const key = new Key();
+const myHouse = new MyHouse(key);
+const person = new Person(key);
+
+myHouse.openDoor(person.getKey());
+myHouse.comeIn(person);
