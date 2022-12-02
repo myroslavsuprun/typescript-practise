6cc5c8f9433969e0c3781dd4502962edf51b10af
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2022-12-02 21:13:10 +0200

    Task completed

--- two-fer.test.ts
+++ two-fer.test.ts
@@ -1,12 +1,12 @@
 import { twoFer } from './two-fer'
 
 describe('TwoFer', () => {
-  xit('no name given', () => {
+  it('no name given', () => {
     const expected = 'One for you, one for me.'
     expect(twoFer()).toEqual(expected)
   })
 
-  xit('a name given', () => {
+  it('a name given', () => {
     const expected = 'One for Alice, one for me.'
     expect(twoFer('Alice')).toEqual(expected)
   })
